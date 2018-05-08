052299c8bba4c5e45423baaa3b34605f88caa52b
Дима <krymskikh@gmail.com>
2018-05-08 15:27:34 +0700

    app v1.1.0

--- src/js/Weather.js
+++ src/js/Weather.js
@@ -6,7 +6,7 @@ import Form from './Weather/Form';
 import Aside from './Weather/Aside';
 import Footer from './Footer';
 
-import {initCities} from './Weather/help';
+import {initCities, fetchW} from './Weather/help';
 
 import xSvg from '../svg/x.svg';
 import wiCloudy from '../svg/wi-cloudy.svg';
@@ -19,30 +19,21 @@ class Weather extends React.Component {
         super(props);
         this.navClass = ['nav', 'nav active'];
         this.state = {
+            selector: 'note',
+            answer: '',
             clText: 'text out'
         };
-        
     }
+    
     componentDidMount() {
-(async () => {
-const initStateCities = await initCities();
-
-console.log(initStateCities);
-console.log(this.state.clText)
-this.props.onInitCities(initStateCities);
-this.setState({
-    clText: this.props.listCities.length ? 'text out' : 'text'
-});
-console.log(this.state.clText)
-})();
-
-//this.props.dispatch({
-//    type: 'INIT',
-//    arr: initStateCities
-//});
+        (async () => {
+            const initStateCities = await initCities();
+            this.props.onInitCities(initStateCities);
+            this.setState({
+                clText: this.props.listCities.length ? 'text out' : 'text'
+            });
+        })();
     }
-    
-    
     
     getIcon(v) {
         if( v < 20 ) return wiDaySunny;
@@ -51,22 +42,59 @@ console.log(this.state.clText)
         if( 80 <= v  ) return wiRain;
     };
     
+    async addCity(v) {
+        if(!v.value) return;
+        
+        let obCity = await fetchW(v.value);
+    
+        if(obCity.cod === 200) {
+            this.setState({
+                selector: 'note good',
+                answer: 'Город добавлен \u2713',
+                clText: 'text out'
+            });
+            // Делаем запись в хранилище
+            const key = obCity.name;
+            const value = obCity.name;
+            localStorage.setItem(key, value);
+            // Добавляем город в store для динамического обновления
+            this.props.onAddCity(obCity);
+        } else {
+            this.setState({
+                selector: 'note bad',
+                answer: 'Город не найден \u2718' 
+            });
+        }
+        // Очищаем поле ввода
+        v.value = '';
+        setTimeout( this.setState.bind(this, {
+            selector: 'note',
+            answer: ''
+        }), 3000);
+    }
+    
     deleteCity(index) {
         return () => {
             // Удаляем город из хранилища
             localStorage.removeItem(this.props.listCities[index].name);
             // Удаляем город из store
             this.props.onDeleteCity(this.props.listCities[index]);
+            // Пишем this.props.listCities.length - 1
+            // потому что this.props.listCities остаётся неизменной в deleteCity
+            this.setState({
+                clText: this.props.listCities.length - 1 ? 'text out' : 'text'
+            });
         };
     };
     
     render() {
         return (
             <div className="weather">
-                <Menu navClass={this.navClass} />
+                <Menu navClass={this.navClass} clText={this.state.clText} />
                 <div className="container main">
                     <h1>Погода в городах мира</h1>
-                    <Form />
+                    <Form onAddCity={this.addCity.bind(this)} />
+                    <span className={this.state.selector}>{this.state.answer}</span>
                     <div className='row'>
                         <main>
                             <div className={this.state.clText}>Нет выбранных городов</div>
@@ -104,11 +132,14 @@ export default connect(
         listCities: state.listCities
     }),
     dispatch => ({
+        onAddCity: ob => {
+            dispatch({type: 'ADD', city: ob});
+        },
         onDeleteCity: ob => {
             dispatch({type: 'DELETE', city: ob});
         },
         onInitCities: arr => {
-            dispatch({type: 'INIT', arr: arr})
+            dispatch({type: 'INIT', arr: arr});
         }
     })
 )(Weather);
--- src/js/Weather/Form.js
+++ src/js/Weather/Form.js
@@ -1,76 +1,30 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-import {fetchW} from './help';
-
-class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selector: 'note',
-            answer: ''
-        };
-    }
+const Form = ({onAddCity}) => {
     
-    async addCity() {
-        if(!this.inputCity.value) return;
-        
-        let obCity = await fetchW(this.inputCity.value);
+    let inputCity = null;
     
-        if(obCity.cod === 200) {
-            this.setState({
-                selector: 'note good',
-                answer: 'Город добавлен \u2713'
-            });
-            // Делаем запись в хранилище
-            const key = obCity.name;
-            const value = obCity.name;
-            localStorage.setItem(key, value);
-            // Добавляем город в store для динамического обновления
-            this.props.onAddCity(obCity);
-        } else {
-            this.setState({
-                selector: 'note bad',
-                answer: 'Город не найден \u2718' 
-            });
-        }
-        // Очищаем поле ввода
-        this.inputCity.value = '';
-        setTimeout( this.setState.bind(this, {
-            selector: 'note',
-            answer: ''
-        }), 3000);
-    }
+    const addCity = () => {
+        onAddCity(inputCity);
+    };
     
-    render() {
-        return (
-            <div className='weather-form'>
-                <p>
-                    Введите название города на английском, чтобы узнать текущую погоду.
-                    Например, Novosibirsk или Moscow.
-                </p>
-                <div className="form-row">
-                    <div className="form-left">
-                        <input type='text' name='city' placeholder='Город' ref={ v => {this.inputCity = v} } />
-                    </div>
-                    <div className="form-right">
-                        <button type='button' onClick={this.addCity.bind(this)}>Получить</button>
-                    </div>
+    return (
+        <div className='weather-form'>
+            <p>
+                Введите название города на английском, чтобы узнать текущую погоду.
+                Например, Novosibirsk или Moscow.
+            </p>
+            <div className="form-row">
+                <div className="form-left">
+                    <input type='text' name='city' placeholder='Город' ref={ v => {inputCity = v} } />
                 </div>
-                <span className={this.state.selector}>{this.state.answer}</span>
+                <div className="form-right">
+                    <button type='button' onClick={addCity}>Получить</button>
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 };
 
 
-export default connect(
-    state => ({
-        cities: state
-    }),
-    dispatch => ({
-        onAddCity: ob => {
-            dispatch({type: 'ADD', city: ob});
-        }
-    })
-)(Form);
+export default Form;
